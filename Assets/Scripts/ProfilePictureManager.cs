1732538b8d18f0b6d85b2e5b6ef1bb011c7a6016
Vince <91570705+Vurs@users.noreply.github.com>
2023-10-25 19:32:26 -0400

    Emergency hotfix
    Fetching profile pictures from AWS completely broke the app on mobile builds, so I had to bundle the profile pictures into the app and build it and fetch the profile pictures locally.

--- Assets/Scripts/ProfilePictureManager.cs
+++ Assets/Scripts/ProfilePictureManager.cs
@@ -10,8 +10,9 @@ using UnityEngine.UI;
 public class ProfilePictureManager : MonoBehaviour
 {
     public Dictionary<string, Texture2D> profilePictures = new Dictionary<string, Texture2D>();
+    public Texture2D[] images;
 
-    private DatabaseReference databaseReference;
+    //private DatabaseReference databaseReference;
 
     private void Awake()
     {
@@ -21,54 +22,60 @@ public class ProfilePictureManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        //FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        //{
+        //    FirebaseApp app = FirebaseApp.DefaultInstance;
+        //    databaseReference = FirebaseDatabase.DefaultInstance.RootReference.Child("profile_pictures");
+
+        //    // Fetch and load profile pictures.
+        //    LoadProfilePictures();
+        //});
+
+        foreach (Texture2D texture in images)
         {
-            FirebaseApp app = FirebaseApp.DefaultInstance;
-            databaseReference = FirebaseDatabase.DefaultInstance.RootReference.Child("profile_pictures");
-
-            // Fetch and load profile pictures.
-            LoadProfilePictures();
-        });
-    }
-
-    void LoadProfilePictures()
-    {
-        databaseReference.GetValueAsync().ContinueWithOnMainThread(task =>
-        {
-            if (task.IsCompleted)
-            {
-                DataSnapshot snapshot = task.Result;
-                profilePictures.Clear();
-
-                foreach (DataSnapshot childSnapshot in snapshot.Children)
-                {
-                    string imageId = childSnapshot.Key;
-                    string imageUrl = childSnapshot.Value.ToString();
-                    StartCoroutine(LoadImage(imageId, imageUrl));
-                }
-            }
-            else
-            {
-                Debug.LogError("Failed to fetch profile pictures: " + task.Exception);
-            }
-        });
-    }
-
-    IEnumerator LoadImage(string imageId, string imageUrl)
-    {
-        UnityWebRequest www = UnityWebRequestTexture.GetTexture(imageUrl);
-        yield return www.SendWebRequest();
-
-        if (www.result == UnityWebRequest.Result.Success)
-        {
-            Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
-            profilePictures.Add(imageId,texture);
-        }
-        else
-        {
-            Debug.LogError("Failed to load image: " + www.error);
+            profilePictures.Add(texture.name, texture);
+            Debug.Log("Added " + texture.name);
         }
     }
+
+    //void LoadProfilePictures()
+    //{
+    //    databaseReference.GetValueAsync().ContinueWithOnMainThread(task =>
+    //    {
+    //        if (task.IsCompleted)
+    //        {
+    //            DataSnapshot snapshot = task.Result;
+    //            profilePictures.Clear();
+
+    //            foreach (DataSnapshot childSnapshot in snapshot.Children)
+    //            {
+    //                string imageId = childSnapshot.Key;
+    //                string imageUrl = childSnapshot.Value.ToString();
+    //                StartCoroutine(LoadImage(imageId, imageUrl));
+    //            }
+    //        }
+    //        else
+    //        {
+    //            Debug.LogError("Failed to fetch profile pictures: " + task.Exception);
+    //        }
+    //    });
+    //}
+
+    //IEnumerator LoadImage(string imageId, string imageUrl)
+    //{
+    //    UnityWebRequest www = UnityWebRequestTexture.GetTexture(imageUrl);
+    //    yield return www.SendWebRequest();
+
+    //    if (www.result == UnityWebRequest.Result.Success)
+    //    {
+    //        Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+    //        profilePictures.Add(imageId,texture);
+    //    }
+    //    else
+    //    {
+    //        Debug.LogError("Failed to load image: " + www.error);
+    //    }
+    //}
 
     public void SetImage(Image targetImage, Texture2D texture)
     {
